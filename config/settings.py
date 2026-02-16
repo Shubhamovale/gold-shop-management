4fa7b8e44bed7183a68b4b1a25cdbcdb24a1f142
Shubham Ovale <shubhamovale12@gmail.com>
2026-02-16 23:17:23 +0530

    Fix AppRegistry error

--- config/settings.py
+++ config/settings.py
@@ -125,16 +125,3 @@ LOGIN_REDIRECT_URL = '/inventory/'
 LOGOUT_REDIRECT_URL = '/login/'
 LOGIN_REDIRECT_URL = '/inventory/dashboard/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-
-import os
-from django.contrib.auth import get_user_model
-
-def create_default_superuser():
-    username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
-    email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
-    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin123")
-
-    User = get_user_model()
-
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username, email, password)
