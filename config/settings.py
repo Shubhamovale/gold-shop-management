bccacaf4ee5f35e2c99465f715f436372cffff5f
Shubham Ovale <shubhamovale12@gmail.com>
2026-02-16 23:05:14 +0530

    Auto create superuser

--- config/settings.py
+++ config/settings.py
@@ -127,13 +127,14 @@ LOGIN_REDIRECT_URL = '/inventory/dashboard/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 import os
-if os.environ.get("RENDER") == "true":
-    from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model
+
+def create_default_superuser():
+    username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
+    email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin123")
+
     User = get_user_model()
 
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            username="admin",
-            email="admin@example.com",
-            password="admin123"
-        )
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
--- config/wsgi.py
+++ config/wsgi.py
@@ -14,3 +14,10 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+from django.conf import settings
+
+try:
+    settings.create_default_superuser()
+except Exception as e:
+    print("Superuser creation skipped:", e)
