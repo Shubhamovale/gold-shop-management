1bd941179ce4fa397d9b3d63f36e338e24065f0f
Shubham Ovale <shubhamovale12@gmail.com>
2026-02-17 14:29:14 +0530

    Added login and registration system

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import register_view
 
 urlpatterns = [
-    path("register/", register_view, name="register"),
+    path('register/', register_view, name='register'),
 ]
