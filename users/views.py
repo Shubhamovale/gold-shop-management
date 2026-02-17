fca52c6a9833afb4701392489c4da1b95c39627b
Shubham Ovale <shubhamovale12@gmail.com>
2026-02-17 14:55:59 +0530

    Fix staff role assignment

--- users/views.py
+++ users/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.auth.models import Group
 
-
-from django.shortcuts import render, redirect
-from .forms import RegisterForm
 
 def register_view(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
 
         if form.is_valid():
-            form.save()
+
+            # SAVE USER AND STORE IN VARIABLE
+            user = form.save()
+
+            # GET STAFF GROUP
+            staff_group = Group.objects.get(name='Staff')
+
+            # ASSIGN USER TO STAFF GROUP
+            user.groups.add(staff_group)
+
             return redirect('login')
+
     else:
         form = RegisterForm()
 
