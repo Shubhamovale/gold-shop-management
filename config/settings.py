9f9f77583e934ccdb39c47f9863bc4b661a71026
Shubham Ovale <shubhamovale12@gmail.com>
2026-03-06 20:24:55 +0530

    Resolve Error For registration

--- config/settings.py
+++ config/settings.py
@@ -75,15 +75,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
+import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'Postgresql12',
-        'HOST': 'postgresql.cpqoywoqg69n.ap-south-1.rds.amazonaws.com',
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
