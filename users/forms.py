5d3109fdba4690988888143f46c407e1a816f62b
Shubham Ovale <shubhamovale12@gmail.com>
2026-03-08 15:12:19 +0530

    Fix registration form and run migrations

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from .models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
@@ -8,4 +7,4 @@ User = get_user_model()
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2', 'role']
